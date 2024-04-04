bee67131721b16ec1791403e3230c0910a06f07a
Guddu Dhananjaya <107747038+Guddu144@users.noreply.github.com>
2024-04-04 18:16:37 +0545

    Update apiClient.js

--- blys-FE/src/apiClient.js
+++ blys-FE/src/apiClient.js
@@ -1,6 +1,6 @@
 import request from "./request";
 
-const API_URL = "http://localhost:3000";
+const API_URL = "https://verification-guddu.onrender.com";
 
 
 export const submitVerificationCode = (payload) =>request('POST', API_URL + '/verification',payload);
